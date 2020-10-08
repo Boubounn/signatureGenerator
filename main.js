bd9e2cce7b4465a50e781f77f77c0d146b01ab9c
unknown <brice.mebarki@gmail.com>
2020-10-08 18:00:44 +0200

    environnement crée et fonctionnel

---
+++ main.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+    el: "#app",
+    data: {
+        inputs: {
+            name: null,
+            firstName: null,
+            phone: null,
+            email: null,
+            adress: null
+        }
+    }
+});
